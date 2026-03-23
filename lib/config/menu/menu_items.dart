7b101248d37767ad44a66b3318ef0e296da69304
Esteban Castillo <ec2818913@gmail.com>
2026-03-23 12:30:09 -0600

    tutorial screen agregada

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -56,4 +56,11 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+
+  MenuItem(
+    title: 'Introduccion a la aplicacion',
+    subTitle: 'Pequeño tutorial introductorio',
+    link: '/tutorial',
+    icon: Icons.accessible_rounded,
+  ),
 ];
